276b86421558ddd62baf3cb24b462292d4954f2d
bhasinn <bhasinn@rose-hulman.edu>
2020-10-29 15:38:07 -0400

    Done.

--- 20-Exam3Practice/src/m3_console_input.py
+++ 20-Exam3Practice/src/m3_console_input.py
@@ -5,11 +5,11 @@ This problem provides practice at: *** INPUT from the CONSOLE. ***
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Derek Whitley, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Neha Bhasin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.  [Note: same _TODO_ as its matching one in module m1.]
+# DONE: 2.  [Note: same _TODO_ as its matching one in module m1.]
 #  Students:
 #  __
 #  These problems have DIFFICULTY and TIME ratings:
@@ -44,7 +44,7 @@ def main():
 def run_test_input_it_all():
     """ Tests the   input_it_all    function. """
     # -------------------------------------------------------------------------
-    # TODO: 3. READ the tests below.  They require that you input specific
+    # DONE: 3. READ the tests below.  They require that you input specific
     #   inputs (although your function should work for any valid inputs).
     #  After you read (and understand) the above, change this _TODO_ to DONE.
     # -------------------------------------------------------------------------
@@ -118,7 +118,7 @@ def input_it_all():
            "4 3.48 7.48 0.93 Peace & LovePeace & LovePeace & LovePeace & Love"
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          The testing code is already written for you (above).
     #  __
     #  HINT: When you add an  import math   statement,
@@ -130,7 +130,20 @@ def input_it_all():
     #    DIFFICULTY:      3
     #    TIME ESTIMATE:  15 minutes.
     # -------------------------------------------------------------------------
-
+    floating_num = float(input("Enter a floating point number: "))
+    number = int(input("Enter a postive integer: "))
+    string = input("Enter a string: ")
+    return_string = ''
+    return_string = return_string + str(number)
+    return_string = return_string + " "
+    return_string = return_string + str(floating_num)
+    return_string = return_string + " "
+    return_string = return_string + str(floating_num + number)
+    return_string = return_string + " "
+    return_string = return_string + str(round(math.sin(floating_num + number), 2))
+    return_string = return_string + " "
+    return_string = return_string + (string * number)
+    return return_string
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
