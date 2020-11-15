e34c90de675e060d920ec1e1e52f646507c6353a
bhasinn <bhasinn@rose-hulman.edu>
2020-11-14 23:47:33 -0500

    Done.

--- 18-LoopsWithinLoops/src/m4_nested_loops_in_sequences.py
+++ 18-LoopsWithinLoops/src/m4_nested_loops_in_sequences.py
@@ -4,8 +4,8 @@ in the context of SEQUENCES OF SUB-SEQUENCES.
 
 Authors: David Mutchler, Sana Ebrahimi, Mohammed Noureddine, Vibha Alangar,
          Matt Boutell, Dave Fisher, Mark Hays, their colleagues, and
-         PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Neha Bhasin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -16,14 +16,14 @@ def main():
     print()
     print("Un-comment and re-comment calls in MAIN one by one as you work.")
 
-    # run_test_sum_numbers()
-    # run_test_multiply_by_c()
+    run_test_sum_numbers()
+    run_test_multiply_by_c()
 
 
 def run_test_sum_numbers():
     """ Tests the    sum_numbers    function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  sum_numbers  function defined below.
     #   Include at least **   4   ** tests (we wrote 3 for you).
     # -------------------------------------------------------------------------
@@ -65,8 +65,12 @@ def run_test_sum_numbers():
     print_actual_result_of_test(expected, actual, test_results)
 
     # -------------------------------------------------------------------------
-    # TODO: 2 (continued): Add your ADDITIONAL test here:
+    # DONE: 2 (continued): Add your ADDITIONAL test here:
     # -------------------------------------------------------------------------
+    expected = 0
+    actual = sum_numbers(([5, 1], [-1, -5]))
+    print('Expected:', expected)
+    print('Actual:', actual)
 
     # SUMMARY of test results:
     print_summary_of_test_results(test_results)
@@ -83,7 +87,7 @@ def sum_numbers(seq_seq):
                     and each item in the subsequences is a number.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #  __
     #  NOTE: This is a classic SEQUENCE of SEQUENCES problem:
@@ -93,6 +97,11 @@ def sum_numbers(seq_seq):
     #             to loop through it in the INNER loop.
     #        -- See   m2r_nested_loops_in_sequences   as needed.
     # -------------------------------------------------------------------------
+    sum = 0
+    for k in range(len(seq_seq)):
+        for j in range(len(seq_seq[k])):
+            sum = sum + seq_seq[k][j]
+    return sum
 
 
 def run_test_multiply_by_c():
@@ -202,10 +211,13 @@ def multiply_by_c(c, sequence_of_lists):
        :type sequence_of_lists:  sequence of lists of numbers
        """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     # -------------------------------------------------------------------------
+    for k in range(len(sequence_of_lists)):
+        for j in range(len(sequence_of_lists[k])):
+            sequence_of_lists[k][j] = sequence_of_lists[k][j] * c
 
 
 ###############################################################################
